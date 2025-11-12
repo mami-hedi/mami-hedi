b8af4f90d2bca5d919924464dc5772cfdc9147be
mami-hedi <hedimemi@gmail.com>
2025-11-12 14:36:17 +0100

    Premier commit - Portfolio Hedi Mami

---
+++ script.js
@@ -0,0 +1,45 @@
+const projects = [
+  {
+    name: "Séjour Médical",
+    image: "https://www.mh-digital-solution.com/wp-content/uploads/2023/05/sejourmedical.jpg",
+    url: "https://www.sejour-medical.fr/"
+  },
+  {
+    name: "Gem by Gwenaelle",
+    image: "https://www.mh-digital-solution.com/wp-content/uploads/2023/06/gem.jpg",
+    url: "https://www.gembygwenaelle.fr/"
+  },
+  {
+    name: "Histoire Zen",
+    image: "https://www.mh-digital-solution.com/wp-content/uploads/2023/06/histoirezen.jpg",
+    url: "https://www.histoire-zen.fr/"
+  },
+  {
+    name: "Petits Pieds Grand Pas",
+    image: "https://www.mh-digital-solution.com/wp-content/uploads/2023/07/petits.jpg",
+    url: "https://petitspiedsgrandpas.com/"
+  },
+  {
+    name: "Clothilde Valade",
+    image: "https://www.mh-digital-solution.com/wp-content/uploads/2023/08/clothilde.jpg",
+    url: "https://www.clothildevalade.com/"
+  },
+  {
+    name: "Fleurs d’Harmonie",
+    image: "https://www.mh-digital-solution.com/wp-content/uploads/2023/09/fleurs.jpg",
+    url: "https://fleursdharmonie.com/"
+  },
+];
+
+const projectList = document.getElementById("project-list");
+
+projects.forEach(p => {
+  const card = document.createElement("div");
+  card.classList.add("project");
+  card.innerHTML = `
+    <img src="${p.image}" alt="${p.name}">
+    <h3>${p.name}</h3>
+    <a href="${p.url}" target="_blank">Visiter le site</a>
+  `;
+  projectList.appendChild(card);
+});
